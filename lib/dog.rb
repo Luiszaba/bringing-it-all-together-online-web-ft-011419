9de62357fc615ab860129f8c217a2d893ab93ee2
Luis Zabala <luis.zabala076@gmail.com>
2019-02-19 23:59:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,7 +12,7 @@ class Dog
   
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NONE EXISTS dogs (
+    CREATE TABLE IF NOT EXISTS dogs (
     id INTEGER PRIMARY KEY,
     name TEXT,
     breed TEXT
