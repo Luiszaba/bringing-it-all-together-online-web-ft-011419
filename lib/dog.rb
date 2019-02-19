ec05c85255b3c873bec709689ffc9584eacd1efd
Luis Zabala <luis.zabala076@gmail.com>
2019-02-19 23:57:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,9 @@ class Dog
   attr_accessor :name, :breed, :id
   
   
-  def initialize(attributes)
-    attributes.each {|key, value| self.sen
+  def initialize(name:, breed:, id:nil)
+    @name = name
+    @breed = breed
+    @id = id
   end
 end
