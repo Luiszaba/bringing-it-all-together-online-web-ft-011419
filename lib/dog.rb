de95b5539a852ca69655295137fe477e19c11ec8
Luis Zabala <luis.zabala076@gmail.com>
2019-02-20 00:42:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,12 @@ class Dog
     pet
   end
   
+  def self.new_from_db
+    id=[0]
+    name=[1]
+    breed=[2]
+    
+  
   def self.find_by_id(id)
     pet_sch = DB[:conn].execute(sql, self.id)[0]
 end
